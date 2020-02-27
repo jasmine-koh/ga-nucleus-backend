4e264ed350718219c35857b5482e2c81206ea54c
jasmine-koh <jazs0515@gmail.com>
2020-02-27 17:30:59 +0800

    update schema type for list model

--- models/lists.js
+++ models/lists.js
@@ -14,7 +14,7 @@ const listSchema = new Schema({
   name: {type: String},
   groups: {type: String},
   shared: {type: Boolean},
-  available: {type: Array},
+  available: [{type: String}],
 });
 
 const List = mongoose.model('List', listSchema);
